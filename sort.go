89d273a0201f686bbd94314f3e35223bd3ef6db5
Artem Andreenko <miolini@gmail.com>
2016-03-11 02:34:14 +0300

    first commit

---
+++ sort.go
@@ -0,0 +1,94 @@
+package easysort
+
+import (
+	"fmt"
+	"reflect"
+	"sort"
+	"strings"
+	"time"
+)
+
+type sortableSlice struct {
+	value     reflect.Value
+	len       int
+	fieldName string
+}
+
+func (s *sortableSlice) Len() int {
+	return s.len
+}
+
+func (s *sortableSlice) Less(i, j int) bool {
+	f1 := s.value.Index(i).FieldByName(s.fieldName)
+	f2 := s.value.Index(j).FieldByName(s.fieldName)
+	if f1.Kind() != f2.Kind() {
+		panic("different elements of slice")
+	}
+	switch f1.Kind() {
+	case reflect.String:
+		return strings.Compare(f1.String(), f2.String()) == -1
+	case reflect.Struct:
+		t1, ok1 := f1.Interface().(time.Time)
+		if !ok1 {
+			break
+		}
+		t2, ok2 := f2.Interface().(time.Time)
+		if !ok2 {
+			f2 = f1
+		}
+		return t1.Before(t2)
+	case reflect.Bool:
+		return !f1.Bool() && f2.Bool()
+	case reflect.Float32:
+		return f1.Float() < f2.Float()
+	case reflect.Float64:
+		return f1.Float() < f2.Float()
+	case reflect.Int:
+		return f1.Int() < f1.Int()
+	case reflect.Int8:
+		return f1.Int() < f1.Int()
+	case reflect.Int16:
+		return f1.Int() < f1.Int()
+	case reflect.Int32:
+		return f1.Int() < f1.Int()
+	case reflect.Int64:
+		return f1.Int() < f1.Int()
+	case reflect.Uint:
+		return f1.Uint() < f2.Uint()
+	case reflect.Uint8:
+		return f1.Uint() < f2.Uint()
+	case reflect.Uint16:
+		return f1.Uint() < f2.Uint()
+	case reflect.Uint32:
+		return f1.Uint() < f2.Uint()
+	case reflect.Uint64:
+		return f1.Uint() < f2.Uint()
+	}
+	panic(fmt.Errorf("unsupported field data type: %s", f1.Kind().String()))
+}
+
+func (s *sortableSlice) Swap(i, j int) {
+	v1 := s.value.Index(i)
+	v2 := s.value.Index(j)
+	i1 := v1.Interface()
+	i2 := v2.Interface()
+	v1.Set(reflect.ValueOf(i2))
+	v2.Set(reflect.ValueOf(i1))
+}
+
+func ByField(v interface{}, fieldName string) {
+	sliceValue := reflect.ValueOf(v)
+	if sliceValue.Kind() != reflect.Slice {
+		panic("value is not a slice")
+	}
+	len := sliceValue.Len()
+	if len < 2 {
+		return
+	}
+	field := sliceValue.Index(0).FieldByName(fieldName)
+	if !field.IsValid() {
+		panic(fmt.Errorf("field not exist: %s", fieldName))
+	}
+	metaSlice := sortableSlice{value: sliceValue, len: len, fieldName: fieldName}
+	sort.Sort(&metaSlice)
+}
---
+++ sort_test.go
@@ -0,0 +1,35 @@
+package easysort
+
+import (
+	"testing"
+	"time"
+)
+
+type TestStruct struct {
+	String  string
+	Time    time.Time
+	Float32 float64
+	Float64 float32
+	Bool    bool
+	Int     int
+	Int8    int8
+	Int16   int16
+	Int32   int32
+	Int64   int64
+	Uint    uint
+	Uint16  uint16
+	Uint32  uint32
+	Uint64  uint64
+	Byte    byte
+	Bytes   []byte
+}
+
+var testA = TestStruct{"a", time.Unix(0, 0), 1.11, 1.11, false, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, []byte("testA")}
+var testB = TestStruct{"b", time.Unix(100000, 0), 2.22, 2.22, false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, []byte("testB")}
+var testC = TestStruct{"c", time.Unix(100000000, 0), 3.33, 3.33, true, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, []byte("testC")}
+
+func TestSortByField(t *testing.T) {
+	stringSlice := []TestStruct{testA, testC, testB}
+	ByField(stringSlice, "Byte")
+	t.Logf("result: %v", stringSlice)
+}
